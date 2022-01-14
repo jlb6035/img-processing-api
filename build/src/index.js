63e65ccbdf5f27a09c037b39964fc78b2190fce4
jlb6035 <jlb6035@gmail.com>
2022-01-14 14:56:09 -0500

    added unit test

--- build/src/index.js
+++ build/src/index.js
@@ -1,15 +1,14 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-var express_1 = __importDefault(require('express'));
-var api_1 = __importDefault(require('../routes/api'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var api_1 = __importDefault(require("../routes/api"));
 var app = (0, express_1.default)();
 var port = 3000;
 app.use('/api', api_1.default);
 app.listen(port, function () {
-  console.log('Server running on port '.concat(port));
+    console.log("Server running on port ".concat(port));
 });
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,5 @@ app.use('/api', routes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
