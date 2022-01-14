f20b4f95f1492138d75dbe1a862fddca4b6a75fb
jlb6035 <jlb6035@gmail.com>
2022-01-14 15:00:21 -0500

    added unit test

---
+++ src/tests/routes/imagesSpec.ts
@@ -0,0 +1,7 @@
+import images from "../../../routes/api/images";
+
+describe('Test Image Services', () => {
+    it('test convert file name',  () => {
+        expect(images.convertFileName());         //Not sure why this isn't working...
+    }
+)});
